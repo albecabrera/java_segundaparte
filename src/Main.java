a3e398d8edfeaacc339f3a5b8d2aef0eae7318d9
albecabrera <albe.cabrera@yahoo.de>
2025-04-01 21:12:24 +0200

    third commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,26 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello Alberto!");
+        // bucle while
+       /* int i = 0; // iterador
 
-        }
+        while (i < 10) {
+            System.out.println("El valor es: " + i);
+            i++; */
+        /*int i= 10;
+
+        while (i > 0) {
+            System.out.println("El valor es: " + i);
+            i--; */
+
+        int i = 5;
+
+        do {
+            System.out.println("El valor es: " + i);
+            i++; // incremento
+        } while (i < 10); // expresión
     }
+}
+
+
+
