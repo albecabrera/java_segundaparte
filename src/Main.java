8e7be034c63bba69cd7537f93aed9bcf986450f6
albecabrera <albe.cabrera@yahoo.de>
2025-04-02 08:47:08 +0200

    quinto cambio

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Usuario {
+    String nombre = "Alberto";
+    String apellido = "Urquiola Cabrera";
+    int edad = 47;
+    String direccion = "Kelterweg, 13, 53125, Bonn";
+    String telefono = "0157768754";
+}
 public class Main {
     public static void main(String[] args) {
+        Usuario usuario1 = new Usuario();
+        Usuario usuario2 = new Usuario();
+
+        System.out.println(usuario1.nombre);
+        System.out.println(usuario1.apellido);
+        System.out.println(usuario1.edad);
+        System.out.println(usuario1.direccion);
+        System.out.println(usuario1.telefono);
+
+
+
+
+
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello Alberto!");
@@ -17,7 +37,7 @@ public class Main {
             System.out.println("El valor es: " + i);
             i--; */
 
-        int i = 5;
+        /* int i = 5;
 
         do {
             System.out.println("El valor es: " + i);
@@ -25,11 +45,12 @@ public class Main {
         } while (i < 10); // expresión
 
         for (int j = 0; i < 5; j++) {
-            System.out.println("El va lor es " + j);
+            System.out.println("El va lor es " + j); */
         }
 
     }
-}
+
+
 
 
 
